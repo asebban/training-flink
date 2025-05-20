5b818d0e469f8d07a4ef06266510d7679c3eaf18
asebban <asebban@omnidata.ma>
2025-05-20 16:58:30 +0100

    ex05: réduction

--- src/main/java/ma/medtech/training/flink/DurationJob.java
+++ src/main/java/ma/medtech/training/flink/DurationJob.java
@@ -1,13 +1,15 @@
 package ma.medtech.training.flink;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.apache.flink.runtime.minicluster.MiniClusterConfiguration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class EmbeddedFlinkJob {
+public class DurationJob {
     public static void main(String[] args) throws Exception {
         Configuration config = new Configuration();
         config.setInteger(RestOptions.PORT, 8081); // Port du Dashboard Web si besoin
@@ -28,9 +30,12 @@ public class EmbeddedFlinkJob {
             config
         );
 
-        env.fromElements(1, 2, 3, 4, 5)
-            .map(x -> x * 2)
-            .print();
+        DataStream<Tuple2<String, Double>> txs = env.fromElements(Tuple2.of("card1", 100.0), Tuple2.of("card1", 200.0), Tuple2.of("card2", 50.0));
+        DataStream<Tuple2<String, Double>> txsReduced = txs
+            .keyBy(t -> t.f0)
+            .reduce((t1, t2) -> Tuple2.of(t1.f0, t1.f1 + t2.f1));
+
+        txsReduced.print();
 
         JobExecutionResult result = env.execute("Job en mode embedded");
         System.out.println("Job execution completed with result: " + result.toString());
