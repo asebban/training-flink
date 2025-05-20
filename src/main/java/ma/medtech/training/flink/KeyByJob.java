0c39004c6b3b817f6193877bbe4869d67bb62035
asebban <asebban@omnidata.ma>
2025-05-20 16:51:59 +0100

    ex04: keyBy function

--- src/main/java/ma/medtech/training/flink/KeyByJob.java
+++ src/main/java/ma/medtech/training/flink/KeyByJob.java
@@ -1,13 +1,16 @@
 package ma.medtech.training.flink;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.apache.flink.runtime.minicluster.MiniClusterConfiguration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-public class EmbeddedFlinkJob {
+public class KeyByJob {
     public static void main(String[] args) throws Exception {
         Configuration config = new Configuration();
         config.setInteger(RestOptions.PORT, 8081); // Port du Dashboard Web si besoin
@@ -28,9 +31,10 @@ public class EmbeddedFlinkJob {
             config
         );
 
-        env.fromElements(1, 2, 3, 4, 5)
-            .map(x -> x * 2)
-            .print();
+        DataStream<String> words = env.fromElements("user1", "user2", "user2", "user2", "user1");
+        DataStream<Tuple2<String, Integer>> wordCount = words.map(word -> Tuple2.of(word, 1))
+                                                        .returns(Types.TUPLE(Types.STRING, Types.INT)).keyBy(t -> t.f0).sum(1);
+        wordCount.print();
 
         JobExecutionResult result = env.execute("Job en mode embedded");
         System.out.println("Job execution completed with result: " + result.toString());
