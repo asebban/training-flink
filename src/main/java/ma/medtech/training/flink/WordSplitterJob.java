54eb7e95b81becb9da012ad8de12c92bdb6643dd
asebban <asebban@omnidata.ma>
2025-05-20 13:16:36 +0100

    Word Splitter Exercise

--- src/main/java/ma/medtech/training/flink/WordSplitterJob.java
+++ src/main/java/ma/medtech/training/flink/WordSplitterJob.java
@@ -1,13 +1,16 @@
 package ma.medtech.training.flink;
 
 import org.apache.flink.api.common.JobExecutionResult;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.runtime.minicluster.MiniCluster;
 import org.apache.flink.runtime.minicluster.MiniClusterConfiguration;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
-public class EmbeddedFlinkJob {
+public class WordSplitterJob{
     public static void main(String[] args) throws Exception {
         Configuration config = new Configuration();
         config.setInteger(RestOptions.PORT, 8081); // Port du Dashboard Web si besoin
@@ -28,9 +31,14 @@ public class EmbeddedFlinkJob {
             config
         );
 
-        env.fromElements(1, 2, 3, 4, 5)
-            .map(x -> x * 2)
-            .print();
+        DataStream<String> sentences = env.fromElements("flink is fast", "Stream processing is cool");
+        DataStream<String> words = sentences.flatMap((String line, Collector<String> out) -> {
+            for (String word : line.split(" ")) {
+                out.collect(word);
+            }
+        }).returns(Types.STRING);
+
+        words.print();
 
         JobExecutionResult result = env.execute("Job en mode embedded");
         System.out.println("Job execution completed with result: " + result.toString());
