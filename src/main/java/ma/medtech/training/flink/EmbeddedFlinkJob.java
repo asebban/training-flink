028d726d32900eb5e6c5a16ee001190a5e360814
asebban <asebban@omnidata.ma>
2025-05-23 12:51:07 +0100

    ex11: démonstration d'évènement en retard

--- src/main/java/ma/medtech/training/flink/EmbeddedFlinkJob.java
+++ src/main/java/ma/medtech/training/flink/EmbeddedFlinkJob.java
@@ -1,40 +1,44 @@
 package ma.medtech.training.flink;
 
-import org.apache.flink.api.common.JobExecutionResult;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.configuration.RestOptions;
-import org.apache.flink.runtime.minicluster.MiniCluster;
-import org.apache.flink.runtime.minicluster.MiniClusterConfiguration;
+import java.time.Duration;
+
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.typeinfo.TypeHint;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
 
 public class EmbeddedFlinkJob {
+
     public static void main(String[] args) throws Exception {
-        Configuration config = new Configuration();
-        config.setInteger(RestOptions.PORT, 8081); // Port du Dashboard Web si besoin
 
-        MiniClusterConfiguration miniClusterConfig = new MiniClusterConfiguration.Builder()
-            .setNumTaskManagers(1)
-            .setNumSlotsPerTaskManager(2)
-            .setConfiguration(config)
-            .build();
-
-        MiniCluster miniCluster = new MiniCluster(miniClusterConfig);
-        miniCluster.start();
-
-        // Créer un environnement de Stream lié au cluster embedded
-        StreamExecutionEnvironment env = StreamExecutionEnvironment.createRemoteEnvironment(
-            "localhost",
-            8081,
-            config
-        );
-
-        env.fromElements(1, 2, 3, 4, 5)
-            .map(x -> x * 2)
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        
+        // 1. Simulate a stream of UserClick events with out-of-order arrival
+        @SuppressWarnings("deprecation")
+        DataStream<UserClick> input = env.addSource(new UserClickSource());
+        
+        // 2. Watermark Strategy: Allow 5 seconds of lateness
+        WatermarkStrategy<UserClick> watermarkStrategy = WatermarkStrategy
+        .<UserClick>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+        .withTimestampAssigner((event, ingestionTime) -> event.getTimestamp());
+        
+        // 3. Apply timestamps and watermarks
+        DataStream<UserClick> withWatermarks = input.assignTimestampsAndWatermarks(watermarkStrategy);
+        
+        // 4. Key by user and apply tumbling window of 10s        
+        withWatermarks
+            .map((UserClick event) -> Tuple2.of(event.getUserId(), 1L))
+            .returns(new TypeHint<Tuple2<String, Long>>() {})
+            .keyBy(value -> value.f0)
+            .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+            .sum(1)
+            .map(windowResult -> "User: " + windowResult.f0 + " - Clicks: " + windowResult.f1)
             .print();
+        
+        env.execute("Flink Event Time Window with Watermarks");
 
-        JobExecutionResult result = env.execute("Job en mode embedded");
-        System.out.println("Job execution completed with result: " + result.toString());
-
-        miniCluster.close();
     }
 }
