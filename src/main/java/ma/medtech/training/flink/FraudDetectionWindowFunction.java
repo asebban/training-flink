426e357e7d02b3e4a9792796257c6331fc88e754
asebban <asebban@omnidata.ma>
2025-05-23 16:20:16 +0100

    ex12: Affichage de l'heure de début et de fin de la fenêtre au format hh:mm:ss

--- src/main/java/ma/medtech/training/flink/FraudDetectionWindowFunction.java
+++ src/main/java/ma/medtech/training/flink/FraudDetectionWindowFunction.java
@@ -4,6 +4,10 @@ import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class FraudDetectionWindowFunction extends ProcessWindowFunction<Transaction, String, String, TimeWindow> {
 
     private Double maxAmount = 0.0;
@@ -26,6 +30,13 @@ public class FraudDetectionWindowFunction extends ProcessWindowFunction<Transact
         for (Transaction txn : transactions) {
             amount += txn.getAmount();
         }
+
+        long start = ctx.window().getStart();
+        long end = ctx.window().getEnd();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(ZoneId.systemDefault());
+        String startFormatted = formatter.format(Instant.ofEpochMilli(start));
+        String endFormatted = formatter.format(Instant.ofEpochMilli(end));
+        System.out.print("Window -> " + startFormatted + "-" + endFormatted + " : ");
         System.out.println("Card Number: " + cardNumber + ", Amount: " + amount);
 
         if (amount >= this.maxAmount) {
