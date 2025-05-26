5ceb6eac1709138a4bba7c46401416070b9caff6
asebban <asebban@omnidata.ma>
2025-05-26 20:05:30 +0100

    step07: Ajout des logs

--- src/main/java/ma/medtech/training/flink/SimpleFlinkJob.java
+++ src/main/java/ma/medtech/training/flink/SimpleFlinkJob.java
@@ -1,10 +1,16 @@
 package ma.medtech.training.flink;
 
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SimpleFlinkJob {
 
+    private static final Logger logger = LoggerFactory.getLogger(SimpleFlinkJob.class);
+
     public static void main(String[] args) throws Exception {
+        logger.info("Starting the Flink job");
+
         // Create the execution environment
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -12,7 +18,12 @@ public class SimpleFlinkJob {
         env.fromElements("Hello, Flink!")
            .print();
 
+        logger.debug("Kafka consumer created and configured");
+        logger.info("Processing stream started");
+
         // Execute the job
         env.execute("Simple Flink Job");
+
+        logger.info("Flink job execution completed");
     }
 }
